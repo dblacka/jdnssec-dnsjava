36bae566ea7692c56290f87e066be03f1d6a49f7
David Blacka <davidb@verisign.com>
2005-09-20 22:13:56 +0000

    build.xml tweaks; svn:ignore

---
+++ org/xbill/DNS/NSEC3Record.java
@@ -0,0 +1,243 @@
+// Copyright (c) 1999-2004 Brian Wellington (bwelling@xbill.org)
+
+package org.xbill.DNS;
+
+import java.io.*;
+import java.util.*;
+
+import org.xbill.DNS.utils.base16;
+import org.xbill.DNS.utils.base32;
+
+/**
+ * Next SECure name 3 - this record contains the next hashed name in an
+ * ordered list of hashed names in the zone, and a set of types for which
+ * records exist for this name. The presence of this record in a response
+ * signifies a negative response from a DNSSEC-signed zone.
+ * 
+ * This replaces the NSEC and NXT records, when used.
+ * 
+ * @author Brian Wellington
+ * @author David Blacka
+ */
+
+public class NSEC3Record extends Record
+{
+
+  private boolean AOFlag;
+  private int     hashAlg;
+  private int     iterations;
+  private byte[]  salt;
+  private byte[]  next;
+  private int     types[];
+
+  NSEC3Record()
+  {
+  }
+
+  Record getObject()
+  {
+    return new NSEC3Record();
+  }
+
+  /**
+   * Creates an NSEC Record from the given data.
+   * 
+   * @param next The following name in an ordered list of the zone
+   * @param types An array containing the types present.
+   */
+  public NSEC3Record(Name name, int dclass, long ttl, boolean AOFlag,
+      int hashAlg, byte[] salt, byte[] next, int[] types)
+  {
+    super(name, Type.NSEC, dclass, ttl);
+    this.AOFlag = AOFlag;
+    this.hashAlg = hashAlg;
+
+    this.salt = new byte[salt.length];
+    System.arraycopy(salt, 0, this.salt, 0, salt.length);
+
+    this.next = new byte[next.length];
+    System.arraycopy(next, 0, this.next, 0, next.length);
+
+    for (int i = 0; i < types.length; i++)
+    {
+      Type.check(types[i]);
+    }
+    this.types = new int[types.length];
+    System.arraycopy(types, 0, this.types, 0, types.length);
+    Arrays.sort(this.types);
+  }
+
+  private int[] listToArray(List list)
+  {
+    int size = list.size();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+      array[i] = ((Integer) list.get(i)).intValue();
+    }
+    return array;
+  }
+
+  void rrFromWire(DNSInput in) throws IOException
+  {
+    int first = in.readU8();
+    AOFlag = (first & 0x80) != 0;
+    hashAlg = first & 0x7F;
+    iterations = in.readU8() << 24 | in.readU16();
+
+    int salt_length = in.readU8();
+    salt = in.readByteArray(salt_length);
+
+    int next_len = 20; // FIXME: should figure this out from the hashAlg.
+    next = in.readByteArray(next_len);
+
+    int lastbase = -1;
+    List list = new ArrayList();
+    while (in.remaining() > 0)
+    {
+      if (in.remaining() < 2)
+        throw new WireParseException("invalid bitmap descriptor");
+      int mapbase = in.readU8();
+      if (mapbase < lastbase)
+        throw new WireParseException("invalid ordering");
+      int maplength = in.readU8();
+      if (maplength > in.remaining())
+        throw new WireParseException("invalid bitmap");
+      for (int i = 0; i < maplength; i++)
+      {
+        int current = in.readU8();
+        if (current == 0) continue;
+        for (int j = 0; j < 8; j++)
+        {
+          if ((current & (1 << (7 - j))) == 0) continue;
+          int typecode = mapbase * 256 + +i * 8 + j;
+          list.add(Mnemonic.toInteger(typecode));
+        }
+      }
+    }
+    types = listToArray(list);
+  }
+
+  void rdataFromString(Tokenizer st, Name origin) throws IOException
+  {
+    int aoflag = st.getUInt8();
+    AOFlag = (aoflag != 0);
+    hashAlg = st.getUInt8(); // FIXME: check this for validity.
+    iterations = (int) st.getUInt32();
+    String salt_hex = st.getString();
+    if (salt_hex.equals("0") || salt_hex.equals("00"))
+    {
+      salt = null;
+    }
+    else 
+    {
+      salt = base16.fromString(salt_hex);
+    }
+    
+    String next_base32 = st.getString();
+    next = base32.fromString(next_base32);
+
+    List list = new ArrayList();
+    while (true)
+    {
+      Tokenizer.Token t = st.get();
+      if (!t.isString()) break;
+      int type = Type.value(t.value);
+      if (type < 0)
+      {
+        throw st.exception("Invalid type: " + t.value);
+      }
+      list.add(Mnemonic.toInteger(type));
+    }
+    st.unget();
+    types = listToArray(list);
+    Arrays.sort(types);
+  }
+
+  /** Converts rdata to a String */
+  String rrToString()
+  {
+    StringBuffer sb = new StringBuffer();
+    sb.append(AOFlag ? '0' : '1');
+    sb.append(' ');
+    sb.append(hashAlg);
+    sb.append(' ');
+    sb.append(iterations);
+    sb.append(salt == null ? "0" : base16.toString(salt));
+    sb.append(' ');
+    sb.append(base32.toString(next).toLowerCase());
+    
+    for (int i = 0; i < types.length; i++)
+    {
+      sb.append(" ");
+      sb.append(Type.string(types[i]));
+    }
+    return sb.toString();
+  }
+
+  /** Returns the next hash */
+  public byte[] getNext()
+  {
+    return next;
+  }
+
+  /** Returns the set of types defined for this name */
+  public int[] getTypes()
+  {
+    int[] array = new int[types.length];
+    System.arraycopy(types, 0, array, 0, types.length);
+    return array;
+  }
+
+  /** Returns whether a specific type is in the set of types. */
+  public boolean hasType(int type)
+  {
+    return (Arrays.binarySearch(types, type) >= 0);
+  }
+
+  static void mapToWire(DNSOutput out, int[] array, int mapbase,
+      int mapstart, int mapend)
+  {
+    int mapmax = array[mapend - 1] & 0xFF;
+    int maplength = (mapmax / 8) + 1;
+    int[] map = new int[maplength];
+    out.writeU8(mapbase);
+    out.writeU8(maplength);
+    for (int j = mapstart; j < mapend; j++)
+    {
+      int typecode = array[j];
+      map[(typecode & 0xFF) / 8] |= (1 << (7 - typecode % 8));
+    }
+    for (int j = 0; j < maplength; j++)
+    {
+      out.writeU8(map[j]);
+    }
+  }
+
+  void rrToWire(DNSOutput out, Compression c, boolean canonical)
+  {
+    out.writeU8((AOFlag ? 0x80 : 0) | (hashAlg & 0x7F));
+    out.writeU8((iterations >> 16) & 0xFF);
+    out.writeU16(iterations & 0xFFFF);
+    out.writeU8(salt == null ? 0 : salt.length);
+    if (salt != null) out.writeByteArray(salt);
+    out.writeByteArray(next);
+
+    if (types.length == 0) return;
+    int mapbase = -1;
+    int mapstart = -1;
+    for (int i = 0; i < types.length; i++)
+    {
+      int base = types[i] >> 8;
+      if (base == mapbase) continue;
+      if (mapstart >= 0)
+      {
+        mapToWire(out, types, mapbase, mapstart, i);
+      }
+      mapbase = base;
+      mapstart = i;
+    }
+    mapToWire(out, types, mapbase, mapstart, types.length);
+  }
+
+}
---
+++ tests/org/xbill/DNS/utils/base32Test.java
@@ -0,0 +1,319 @@
+// -*- Java -*-
+//
+// Copyright (c) 2005, Matthew J. Rutherford <rutherfo@cs.colorado.edu>
+// Copyright (c) 2005, University of Colorado at Boulder
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+// 
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+// 
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+// 
+// * Neither the name of the University of Colorado at Boulder nor the
+//   names of its contributors may be used to endorse or promote
+//   products derived from this software without specific prior written
+//   permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+package org.xbill.DNS.utils;
+
+import junit.framework.TestCase;
+
+public class base32Test extends TestCase
+{
+  public base32Test(String name)
+  {
+    super(name);
+  }
+
+  public void test_toString_empty()
+  {
+    byte[] data = new byte[0];
+    String out = base32.toString(data);
+    assertEquals("", out);
+  }
+
+  public void test_toString_basic1()
+  {
+    byte[] data = {0};
+    String out = base32.toString(data);
+    assertEquals("AA======", out);
+  }
+
+  public void test_toString_basic2()
+  {
+    byte[] data = {0, 0};
+    String out = base32.toString(data);
+    assertEquals("AAAA====", out);
+  }
+
+  public void test_toString_basic3()
+  {
+    byte[] data = {0, 0, 1};
+    String out = base32.toString(data);
+    assertEquals("AAAAC===", out);
+  }
+
+  public void test_toString_basic4()
+  {
+    byte[] data = {(byte) 0xFC, 0, 0};
+    String out = base32.toString(data);
+    assertEquals("7QAAA===", out);
+  }
+
+  public void test_toString_basic5()
+  {
+    byte[] data = {(byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+    String out = base32.toString(data);
+    assertEquals("77776===", out);
+  }
+
+  public void test_toString_basic6()
+  {
+    byte[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    String out = base32.toString(data);
+    assertEquals("AEBAGBAFAYDQQCI=", out);
+  }
+
+  public void test_formatString_empty1()
+  {
+    String out = base32.formatString(new byte[0], 5, "", false);
+    assertEquals("", out);
+  }
+
+  public void test_formatString_shorter()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 17, "", false);
+    assertEquals("AEBAGBAFAYDQQCI=", out);
+  }
+
+  public void test_formatString_sameLength()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 16, "", false);
+    assertEquals("AEBAGBAFAYDQQCI=", out);
+  }
+
+  public void test_formatString_oneBreak()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 14, "", false);
+    assertEquals("AEBAGBAFAYDQQC\nI=", out);
+  }
+
+  public void test_formatString_twoBreaks1()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 7, "", false);
+    assertEquals("AEBAGBA\nFAYDQQC\nI=", out);
+  }
+
+  public void test_formatString_twoBreaks2()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 6, "", false);
+    assertEquals("AEBAGB\nAFAYDQ\nQCI=", out);
+  }
+
+  public void test_formatString_shorterWithPrefix()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI="(16 chars)
+    String out = base32.formatString(in, 16, "!_", false);
+    assertEquals("!_AEBAGBAFAYDQQCI=", out);
+  }
+
+  public void test_formatString_sameLengthWithPrefix()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 16, "!_", false);
+    assertEquals("!_AEBAGBAFAYDQQCI=", out);
+  }
+
+  public void test_formatString_oneBreakWithPrefix()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 10, "!_", false);
+    assertEquals("!_AEBAGBAFAY\n!_DQQCI=", out);
+  }
+
+  public void test_formatString_twoBreaks1WithPrefix()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 7, "!_", false);
+    assertEquals("!_AEBAGBA\n!_FAYDQQC\n!_I=", out);
+  }
+
+  public void test_formatString_twoBreaks2WithPrefix()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 6, "!_", false);
+    assertEquals("!_AEBAGB\n!_AFAYDQ\n!_QCI=", out);
+  }
+
+  public void test_formatString_shorterWithPrefixAndClose()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 17, "!_", true);
+    assertEquals("!_AEBAGBAFAYDQQCI= )", out);
+  }
+
+  public void test_formatString_sameLengthWithPrefixAndClose()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 16, "!_", true);
+    assertEquals("!_AEBAGBAFAYDQQCI= )", out);
+  }
+
+  public void test_formatString_oneBreakWithPrefixAndClose()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 10, "!_", true);
+    assertEquals("!_AEBAGBAFAY\n!_DQQCI= )", out);
+  }
+
+  public void test_formatString_twoBreaks1WithPrefixAndClose()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 7, "!_", true);
+    assertEquals("!_AEBAGBA\n!_FAYDQQC\n!_I= )", out);
+  }
+
+  public void test_formatString_twoBreaks2WithPrefixAndClose()
+  {
+    byte[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // "AEBAGBAFAYDQQCI=" (16 chars)
+    String out = base32.formatString(in, 6, "!_", true);
+    assertEquals("!_AEBAGB\n!_AFAYDQ\n!_QCI= )", out);
+  }
+
+  private void assertEquals(byte[] exp, byte[] act)
+  {
+    assertEquals(exp.length, act.length);
+    for (int i = 0; i < exp.length; ++i)
+    {
+      assertEquals(exp[i], act[i]);
+    }
+  }
+
+  public void test_fromString_empty1()
+  {
+    byte[] data = new byte[0];
+    byte[] out = base32.fromString("");
+    assertEquals(data, out);
+  }
+
+  public void test_fromString_basic1()
+  {
+    byte[] exp = {0};
+    byte[] out = base32.fromString("AA");
+    assertEquals(exp, out);
+  }
+
+  public void test_fromString_basic1_lc()
+  {
+    byte[] exp = {0};
+    byte[] out = base32.fromString("aa");
+    assertEquals(exp, out);
+  }
+  
+  public void test_fromString_basic1_padded()
+  {
+    byte[] exp = {0};
+    byte[] out = base32.fromString("AA======");
+    assertEquals(exp, out);
+  }
+  public void test_fromString_basic2()
+  {
+    byte[] exp = {0, 0};
+    byte[] out = base32.fromString("AAAA");
+    assertEquals(exp, out);
+    out = base32.fromString("aaaa");
+    assertEquals(exp, out);
+    out = base32.fromString("aaaa====");
+    assertEquals(exp, out);
+  }
+
+  public void test_fromString_basic3()
+  {
+    byte[] exp = {0, 0, 1};
+    byte[] out = base32.fromString("AAAAC");
+    assertEquals(exp, out);
+    out = base32.fromString("aaAaC");
+    assertEquals(exp, out);
+    out = base32.fromString("aaaac===");
+    assertEquals(exp, out);
+  }
+
+  public void test_fromString_basic4()
+  {
+    byte[] exp = {(byte) 0xFC, 0, 0};
+    byte[] out = base32.fromString("7qaaa");
+    assertEquals(exp, out);
+    out = base32.fromString("7QAAA===");
+    assertEquals(exp, out);
+  }
+
+  public void test_fromString_basic5()
+  {
+    byte[] exp = {(byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+    byte[] out = base32.fromString("77776");
+    assertEquals(out, exp);
+  }
+
+  public void test_fromString_basic6()
+  {
+    byte[] exp = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    byte[] out = base32.fromString("AEBAGBAFAYDQQCI");
+    assertEquals(out, exp);
+    out = base32.fromString("aebagbafaydqqci=");
+  }
+
+  public void test_fromString_invalid1()
+  {
+    byte[] out = base32.fromString("AAA");
+    assertNull(out);
+  }
+
+  public void test_fromString_invalid2()
+  {
+    byte[] out = base32.fromString("AAAAAA");
+    assertNull(out);
+  }
+
+  public void test_fromString_invalid3()
+  {
+    byte[] out = base32.fromString("A");
+    assertNull(out);
+  }
+
+  public void test_fromString_invalid4()
+  {
+    byte[] out = base32.fromString("01");
+    assertNull(out);
+  }
+
+  public void test_fromString_invalid5()
+  {
+    byte[] out = base32.fromString("AA=");
+    assertNull(out);
+  }
+
+}
